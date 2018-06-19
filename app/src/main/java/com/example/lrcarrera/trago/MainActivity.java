03ec54f8da0fcc5dc32db21c495161e154107ae9
lrcarrera <drewico@gmail.com>
2018-06-19 16:24:33 +0200

    Detect position on Earth solved

--- app/src/main/java/com/example/lrcarrera/trago/MainActivity.java
+++ app/src/main/java/com/example/lrcarrera/trago/MainActivity.java
@@ -1,13 +1,116 @@
 package com.example.lrcarrera.trago;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.SQLOutput;
+
+public class MainActivity extends Activity {
+
+    private String provider;
+    LocationManager mLocationManager;
+    String preferredProvider;
+    float latitude, longitude;
+    TextView latituteField;
+    TextView longitudeField;
+    int LOCATION_REFRESH_TIME = 1;
+    int LOCATION_REFRESH_DISTANCE = 1;
+
+
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //latituteField = (TextView) findViewById(R.id.TextView);
+        //longitudeField = (TextView) findViewById(R.id.long_lat);
+
+        GPSTracker gpsTracker = new GPSTracker(this);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
+        if (gpsTracker.getIsGPSTrackingEnabled())
+        {
+            String stringLatitude = String.valueOf(gpsTracker.latitude);
+            textview = (TextView)findViewById(R.id.fieldLatitude);
+            textview.setText(stringLatitude);
+
+            String stringLongitude = String.valueOf(gpsTracker.longitude);
+            textview = (TextView)findViewById(R.id.fieldLongitude);
+            textview.setText(stringLongitude);
+
+            String country = gpsTracker.getCountryName(this);
+            textview = (TextView)findViewById(R.id.fieldCountry);
+            textview.setText(country);
+
+            String city = gpsTracker.getLocality(this);
+            textview = (TextView)findViewById(R.id.fieldCity);
+            textview.setText(city);
+
+            String postalCode = gpsTracker.getPostalCode(this);
+            textview = (TextView)findViewById(R.id.fieldPostalCode);
+            textview.setText(postalCode);
+
+            String addressLine = gpsTracker.getAddressLine(this);
+            textview = (TextView)findViewById(R.id.fieldAddressLine);
+            textview.setText(addressLine);
+        }
+        else
+        {
+
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gpsTracker.showSettingsAlert();
+        }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    System.out.println("TENGO K ENTRAR");
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.varna_lab_geo_locations, menu);
+        return true;
+    }
+
+
+
+
+
+
 }
